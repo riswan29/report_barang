2fb988fd5dce4a9d1c1b7adb609822e33aa6fb88
riswan29 <wandra290401@gmail.com>
2023-06-15 10:57:56 +0700

    add db user dan buat kode report

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
 class Report(models.Model):
@@ -7,7 +8,7 @@ class Report(models.Model):
         ('sedang', 'Sedang Diproses'),
         ('selesai', 'Selesai Diproses'),
     )
-
+    id = models.AutoField(primary_key=True)
     n_pelapor = models.CharField(max_length=100)
     j_pelapor = models.CharField(max_length=100)
     dep_pelapor = models.CharField(max_length=100)
@@ -18,4 +19,42 @@ class Report(models.Model):
     tgl_lapor = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.n_pelapor
+        return f"KD{self.id:03}"
+    class Meta:
+        ordering = ['id']
+    def save(self, *args, **kwargs):
+        if not self.id:
+            last_report = Report.objects.order_by('id').first()
+            if last_report:
+                last_id = last_report.id
+                self.id = last_id + 1
+            else:
+                self.id = 1
+        super().save(*args, **kwargs)
+
+class User(AbstractUser):
+    LEVEL_CHOICES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='user_images/')
+    status = models.CharField(max_length=100, choices=LEVEL_CHOICES)
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='custom_user_set',
+        related_query_name='custom_user'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='custom_user_set',
+        related_query_name='custom_user'
+    )
+
+    def __str__(self):
+        return self.username
